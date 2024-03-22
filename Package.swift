4b7825216c1a28fa99753753b5921e70bb5a51f1
Jay Lyerly <1743037+jaylyerly@users.noreply.github.com>
2024-03-22 16:26:26 -0400

    Initial import

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HidApi",
+    defaultLocalization: "en",
+    platforms: [.macOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "HidApi",
+            targets: ["HidApi"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "HidApi",
+            dependencies: [],
+            path: "./hidapi",
+            sources: [
+                "mac/hid.c",
+            ],
+            publicHeadersPath: "hidapi",
+            linkerSettings: [
+                .linkedFramework("IOKit"),
+                .linkedFramework("CoreFoundation"),
+            ]
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HidApiSpm
-SPM wrapper for the HID API library
+SPM wrapper for the [HID API library](https://github.com/libusb/hidapi).
+
+The library is accessed via a git submodule checked out at `hidapi`.
